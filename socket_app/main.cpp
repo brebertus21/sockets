a4ed318a6cc0f86eb26cb4d6cbade44717438f3a
Benjamin Rebertus <benjaminrebertus@benjamins-macbook-air.local>
2016-04-22 23:37:13 -0400

    client

--- socket_app/main.cpp
+++ socket_app/main.cpp
@@ -93,6 +93,8 @@ int main(int argc, const char * argv[]) {
         addr.sin_port = htons(portnum);
 
         connect(sock, (sockaddr*)&addr, sizeof(addr));
+        
+        client(sock);
 
     }
     
