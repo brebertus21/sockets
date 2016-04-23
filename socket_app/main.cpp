641d3666c9ec7759c9e3fe4ce1d457fba3a6d14d
Benjamin Rebertus <benjaminrebertus@benjamins-macbook-air.local>
2016-04-22 20:51:21 -0400

    Initial Commit

---
+++ socket_app/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  socket_app
+//
+//  Created by Benjamin Rebertus on 4/22/16.
+//  Copyright © 2016 Benjamin Rebertus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
